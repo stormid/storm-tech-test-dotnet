4a0b4dd2ae1c64e65569ce2a4dd04c0f20170206
Geoff Stockham <geoff@stormid.com>
2022-06-29 12:22:15 +0100

    Add more tests

---
+++ tests/Storm.TechTask.Api.IntegrationTests/Endpoints/HttpClientUtils.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Storm.TechTask.Api.IntegrationTests.Endpoints
+{
+    public static class HttpClientUtils
+    {
+        public static async Task<HttpResponseMessage> PostAsync(this HttpClient httpClient, string url, object payload)
+        {
+            return await httpClient.PostAsync(url, NewHttpContent(payload));
+        }
+
+        private static HttpContent? NewHttpContent(object content)
+        {
+            HttpContent? httpContent = null;
+
+            if (content != null)
+            {
+                var ms = new MemoryStream();
+                SerializeJsonIntoStream(content, ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                httpContent = new StreamContent(ms);
+                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
+
+            return httpContent;
+        }
+
+        private static void SerializeJsonIntoStream(object value, Stream stream)
+        {
+            using (var jtw = new Utf8JsonWriter(stream))
+            {
+                var jsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                jsonOptions.Converters.Add(new JsonStringEnumConverter());
+                JsonSerializer.Serialize(jtw, value, jsonOptions);
+                jtw.Flush();
+            }
+        }
+    }
+}
---
+++ tests/Storm.TechTask.Api.IntegrationTests/Endpoints/Projects/CreateEndpointTests.cs
@@ -0,0 +1,96 @@
+﻿
+using IdentityModel.Client;
+
+using Storm.TechTask.Api.Endpoints.Project;
+using Storm.TechTask.Api.IntegrationTests.Utilities;
+using Storm.TechTask.Core.ProjectAggregate;
+using Storm.TechTask.Core.ProjectAggregate.Commands;
+using Storm.TechTask.Core.ProjectAggregate.Exceptions;
+using Storm.TechTask.SharedKernel.Authorization;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Storm.TechTask.Api.IntegrationTests.Endpoints.Projects
+{
+    [Collection("Sequential")]
+    public class CreateEndpointTests : BaseEndpointFixture
+    {
+        public CreateEndpointTests(ITestOutputHelper output, CustomWebApplicationFactory factory) : base(output, factory)
+        {
+        }
+
+        [Fact]
+        public async Task CreatesProject()
+        {
+            // Arrange
+            this.HttpClient.SetBearerToken(await this.TokenIssuer.GetNewToken(AppRole.ProjectAdmin));
+
+            // Act
+            var response = await this.HttpClient.PostAsync($"/Projects", new CreateProject.Command("name", ProjectCategory.Development, true));
+
+            // Assert
+            await response.ShouldBeSuccess().WithObjectPayload(new ProjectDto(await response.GetJsonIntProp("id"), "name"));
+            var expected = NewProject()
+                .Set(p => p.Id, await response.GetJsonIntProp("id"))
+                .Set(p => p.Name, "name")
+                .Set(p => p.Category, ProjectCategory.Development)
+                .Set(p => p.InternalOnly, true)
+                .Set(p => p.Status, ProjectStatus.Open).Build();
+            await expected.ShouldBeInDb();
+        }
+
+        [Fact]
+        public async Task ReturnsBadRequestIfNameNotUnique()
+        {
+            // Arrange
+            var existing = await NewProject().BuildAndPersist();
+            this.HttpClient.SetBearerToken(await this.TokenIssuer.GetNewToken(AppRole.ProjectAdmin));
+
+            // Act
+            var response = await this.HttpClient.PostAsync($"/Projects", new CreateProject.Command(existing.Name, ProjectCategory.Development, true));
+
+            // Act & Assert
+            await response.ShouldBeBusinessRuleException(new ProjectWithNameAlreadyExistsException(existing.Name));
+        }
+
+        [Theory]
+        [AllRolesExcept(AppRole.ProjectAdmin)]
+        public async Task DoesNotCreateProjectWhenInRole(AppRole role)
+        {
+            // Arrange
+            var projectName = "Test Project";
+            HttpClient.SetBearerToken(await this.TokenIssuer.GetNewToken(role));
+
+            var data = new { Name = projectName };
+
+            // Act
+            var response = await HttpClient.PostAsync("/Projects", data);
+
+            // Assert
+            response.ShouldBeNotAuthorised();
+        }
+
+        [Fact]
+        public async Task ReturnsBadRequestIfNameIsNull()
+        {
+            HttpClient.SetBearerToken(await this.TokenIssuer.GetNewToken(AppRole.ProjectAdmin));
+
+            var data = new { Name = null as string };
+
+            var response = await HttpClient.PostAsync("/Projects", data);
+
+            await response.ShouldBeInvalidCommand();
+        }
+
+        [Fact]
+        public async Task ShouldBeUnauthorized()
+        {
+            // Act
+            var response = await this.HttpClient.PostAsync($"/Projects", new CreateProject.Command("name", ProjectCategory.Development, true));
+
+            // Assert
+            response.ShouldBeNotAuthenticated();
+        }
+    }
+}
